183f9b653ca26222e6006a13bc97691d1f39af34
Gabriel Duenas <duenasdevelops@gmail.com>
2022-08-29 22:00:32 -0400

    define player class and print game board function

---
+++ tic_tac_toe/game.py
@@ -0,0 +1,21 @@
+class TicTacToe:
+    def __init__(self):
+        # we will use a single list to represent 3x3 board
+        self.board = [' ' for _ in range(9)]
+        # keep track of winner!
+        self.current_winner = None
+    
+    def print_board(self):
+        for row in [self.board[i*3:(i+1)*3] for i in range(3)]:
+            print('| ' + ' | '.join(row) + ' |')
+
+    @staticmethod
+    def print_board_nums():
+        # 0 | 1 | 2 etc (tells us what number corresponds to what box)
+        number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
+        for row in number_board:
+            print('| ' + '  | '.join(row) + ' |')
+        
+    def available_moves(self):
+        return [i for i, spot in enumerate(self.board) if spot == ' ']
+        
---
+++ tic_tac_toe/player.py
@@ -0,0 +1,33 @@
+import math
+import random
+
+class Player:
+    def __init__(self, letter):
+        # letter is x or o
+        self.letter = letter
+
+    # we want all players to get thier next move 
+    def get_move(self, game):
+        pass
+
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        # get a random valid spot for our next move
+        square = random.choice(game.available_moves())
+        return square
+
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+    
+    def get_move(self, game):
+        valid_square = False
+        val = None 
+        while not valid_square:
+            square = input(self.letter + '\'s turn. Input move (0-9):')
+            # we're going to check that this is a correct value by tring to cast 
+            # it to an integer, and if it's not, then we say its invalid
+            # if that spot is not availble on the board, we also say its invalid
